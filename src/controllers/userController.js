311f61034b7c5c28fb23bcd12b9d5621d907172c
grace <grace.mika0@gmail.com>
2022-10-22 00:30:26 +0900

    [feat] Sign In

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const { userService } = require('../services');
 
 const signUp = catchAsync(async (req, res) => {
@@ -6,4 +8,17 @@ const signUp = catchAsync(async (req, res) => {
     await userService.signUp( userName,password,email, date_of_birth);
 
     res.status(201).json({ message : "signUp success!" });
-})
+});
+
+const signIn = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+
+  const accessToken = awiait userService.signIn(email, password)
+
+  res.status(200).json( { accessToken })
+});
+
+module.exports = {
+  signUp,
+  signIn
+}
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,12 @@ const { userDao } = require('../models');
 const signUp = async(userName, email, password, date_of_birth) => {
 
 }
+
+const signIn = async (userName, password) => {
+
+}
+
 module.exports = {
-  signUp
+  signUp,
+  signIn
 }
