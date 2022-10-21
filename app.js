36b4c7125f152a98501f5eef740d64c87f4924a7
grace <grace.mika0@gmail.com>
2022-10-21 17:37:14 +0900

    [Test]test git branch signUp before layered

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const app = express();
+const createApp = () => {
+  const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan('dev'));
+  
+  app.get('/ping', function (req, res, next) {
+    res.json({ message : 'pong' })
+  })
+  return app;
+};
 
-app.get('/ping', function (req, res, next) {
-  res.json({ message : 'pong freeeedom!!!!!!!!!' });
-});
-
-app.listen(3000, () => {
-  console.log(`Listening to request on 127.0.0.1:3000`);
-});
