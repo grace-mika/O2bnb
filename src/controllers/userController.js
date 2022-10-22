c7322a4c8101ec3cd7de640f65f5c8b01ef017e3
grace <grace.mika0@gmail.com>
2022-10-23 04:18:00 +0900

    [FEAT] Sign In

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,12 @@
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const { userService } = require('../services');
-
-const signUp = catchAsync(async (req, res) => {
-  const { userName, password, email, date_of_birth } = req.body;
-  
-    await userService.signUp( userName,password,email, date_of_birth);
-
-    res.status(201).json({ message : "signUp success!" });
-});
+const userService = require('../services/userService');
 
 const signIn = catchAsync(async (req, res) => {
   const { email, password } = req.body;
 
-  const accessToken = awiait userService.signIn(email, password)
+  const accessToken = await userService.signIn(email, password)
 
   res.status(200).json( { accessToken })
 });
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,33 @@
+const bcrypt = require('bcrypt');
+
 const { userDao } = require('../models');
+const validator = require('../utils/validator');
 
-const signUp = async(userName, email, password, date_of_birth) => {
+const hashPassword = async (password) => {
+  const saltRound = 10;
+  const salt = await bcrypt.genSalt(saltRound);
 
+  return await bcrypt.hash(password, salt)
 }
 
-const signIn = async (userName, password) => {
+const signUp = async( userName, password, email, date_of_bith ) => {
+    validator.validateEmail(email);
+    validator.validatePassword(password);
 
+    const checkOverlap = await userDao.getUserByEmail(email)
+
+    if (checkOverlap) {
+        const error = new Error('INVALID_USER')
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    return await userDao.createUser( userName, password, email, date_of_bith )
 }
+
 
 module.exports = {
-  signUp,
-  signIn
+  signUp
 }
