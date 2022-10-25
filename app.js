22ba1622c05ccc0ff1ec4bc1748f109a0d4e53bc
grace <grace.mika0@gmail.com>
2022-10-26 07:09:22 +0900

    test

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+require("dotenv").config();
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
-const routers = require("./src/routers");
+const cors = require('cors');
+const { globalErrorHandler } = require('./src/utils/error');
 
-const createApp = () => {
-  const app = express();
+const app = express();
+const PORT = process.env.PORT;
+const router = require('./src/routers');
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(morgan('dev'));
-  
-  app.get('/ping', function (req, res, next) {
-    res.json({ message : 'pong' })
-  })
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(router);
+app.use(globalErrorHandler);
 
-  app.use(routers);
+app.get('/ping', function(req, res, next) {
+  res.json({message : 'pong'})
+})
 
-  return app;
-};
-
-module.exports = { createApp };
+app.listen(PORT, () => {console.log(`Listening to request on 127.0.0.1:${PORT}`);});
--- src/routers/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const userRouter = require('./userRouter');
-
-router.use('/user', userRouter);
-
-module.exports = router;
